57cc3d87e294fc3aac82ebd8175d683f5aeb2a89
SadLoneWolf <djordjeandjic99@gmail.com>
2021-04-19 19:38:19 +0200

    Frame added

--- kggl2/kggl2.cpp
+++ kggl2/kggl2.cpp
@@ -6,9 +6,12 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 using namespace Gdiplus;
 void draw(HDC hdc);
 void Display(HDC hdc);
+void DrawCar(HDC hdc);
 
 #pragma comment(lib,"GdiPlus.lib")
 Image* img;
+UINT frameIndex;
+UINT frameCount;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow) {
 
@@ -55,8 +58,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
    }
 
 
-
-
    ShowWindow(hWnd, nCmdShow);
 
    MSG msg;
@@ -64,29 +65,43 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
        TranslateMessage(&msg);
        DispatchMessage(&msg);
    }
-   delete img; 
    
    GdiplusShutdown(gdiplusTokens);
 }
 
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-   switch (msg) {
-   case WM_RBUTTONDBLCLK:
-
-       return 0;
+    switch (msg) {
+    case WM_CREATE:
+    {
+        img = Image::FromFile(L"C://Users//SadLoneWolf//source//repos//kggl2//kggl2//car.gif");
+        if (NULL == img)return -1;
+        frameIndex = 0;
+        frameCount = img->GetFrameCount(&FrameDimensionTime);
+        SetTimer(hWnd, 1, 40, NULL);
+        return 0;
+    }
+    case WM_TIMER:
+    {
+        frameIndex = (frameIndex + 1) % frameCount;
+        img->SelectActiveFrame(&FrameDimensionTime, frameIndex);
+        InvalidateRect(hWnd, NULL, false);
+        return 0;
+    }
    case WM_PAINT:
    {
        PAINTSTRUCT ps;
        HDC hDc = BeginPaint(hWnd, &ps);
        FillRect(hDc, &ps.rcPaint, (HBRUSH)(COLOR_WINDOW + 1));
        //draw(hDc);
-       Display(hDc);
+      // Display(hDc);
+       DrawCar(hDc);
        EndPaint(hWnd, &ps);
        return 0;
    }
    case WM_DESTROY:
    {
+       delete img;
        PostQuitMessage(0);
        return 0;
    }
@@ -95,58 +110,32 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
    return DefWindowProc(hWnd, msg, wParam, lParam);
 }
 
-void draw(HDC hdc)
+void DrawCar(HDC hdc)
 {
-    Region tireRegion(Rect(1, 1, 1, 1));
-    Region fontRegion(Rect(0, 0, 0, 0));
 
-    Region infinite;
+    Graphics gf(hdc);
 
-   Point point;
-   Rect rect(340, 400, 70, 70);
-   Rect rect2(700, 400, 70, 70);
-   RectF MainRect(320, 330, 500, 70);
-   Graphics gf(hdc);
+    gf.DrawImage(img, 60, 60, 160, 260);
    gf.SetSmoothingMode(SmoothingModeAntiAlias);
-   Pen pen(Color(255, 0, 0, 0), 10.f); 
-   float comp[6] = {
-       0.0f,0.2f,
-       0.3f,0.7f,
-       0.9f,1.0f
-   };
-   pen.SetCompoundArray(comp, 6);
-   SolidBrush brush(Color(255, 0, 0, 0)); 
-   SolidBrush brush2(Color(255, 0, 191, 255)); 
-   SolidBrush brushLights(Color(255, 255, 215, 0)); 
-   SolidBrush brushLightsBack(Color(255, 255, 140, 0)); 
-   LinearGradientBrush linearBrush(MainRect, Color(255, 178, 34, 34), Color(255, 128, 0, 0), 90, FALSE);
-
-  
-   gf.FillRectangle(&linearBrush, 320, 330, 500, 70);
-   gf.FillEllipse(&brush, rect);
-   gf.FillEllipse(&brush, rect2);
-   gf.FillRectangle(&brushLightsBack, 325, 330, 15, 6); 
-   gf.FillRectangle(&brushLights, 800, 330, 15, 6);
-  
-   gf.SetClip(&tireRegion);
-   gf.DrawRectangle(&pen, 325, 330, 15, 6);
-   gf.DrawEllipse(&pen, rect);
-   gf.DrawRectangle(&pen, MainRect);
-   gf.DrawRectangle(&pen, 800, 330, 15, 6);
-   gf.DrawEllipse(&pen, rect2);
-
-   Point points[6] =
+   Pen mainPen(Color::Red);
+      Point points[6] =
    {
         Point(350,330), Point(380,240),
         Point(380,240), Point(720,240),
         Point(720,240), Point(780,330)
    };
-   gf.SetClip(&infinite);
-   gf.DrawLines(&pen, points, 6);
-   gf.DrawLine(&pen, 570, 240, 570, 330);
+  
+   Matrix mx;
 
-
+       for (int j = 0; j < frameIndex; j++)
+       {
+           mx.Translate(10.f, 0.f);
+           mx.TransformPoints(points, 6);
+           gf.DrawLines(&mainPen, points, 6);
+       }
 }
+
+
 //Пример аффиного преобразования
 //
 //void Display(HDC hdc)
